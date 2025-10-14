3b8091a6080f1f0985f77800f3bdc37b8ee6205d
Walter <worellana09@gmail.com>
2025-10-14 11:49:11 -0400

    about me but using f string method

---
+++ python1/about_me2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+my_name = "Walter"
+favorite_color = "Purple"
+activity = "Sleeping"
+
+print(f'My name is {my_name}, my favorite color is {favorite_color}, and my favorite activity is {activity}.')
+
+
