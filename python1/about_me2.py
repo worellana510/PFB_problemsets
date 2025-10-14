76def5b9fae38f5789643b6ea50439216a9cbb2a
Walter <worellana09@gmail.com>
2025-10-14 12:14:44 -0400

    updated version

--- python1/about_me2.py
+++ python1/about_me2.py
@@ -6,4 +6,6 @@ activity = "Sleeping"
 
 print(f'My name is {my_name}, my favorite color is {favorite_color}, and my favorite activity is {activity}.')
 
+print('My name is', my_name,'and my favorite color is',favorite_color,'.', sep="")
+
 
