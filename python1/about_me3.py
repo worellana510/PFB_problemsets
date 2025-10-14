daa888fe008a86a52c6642dea32901ba0a4e39a1
Walter <worellana09@gmail.com>
2025-10-14 12:12:36 -0400

    using the sys.arg method

---
+++ python1/about_me3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+my_name = sys.argv[1]
+favorite_color = sys.argv[2]
+activity = sys.argv[3]
+
+print('My name is',my_name, 'my favorite color is',favorite_color, 'and my favorite activity is',activity,)
