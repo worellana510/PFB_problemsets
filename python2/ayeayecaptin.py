27e693bb2397c92af559587e1e3d2191f37a564a
Walter <worellana09@gmail.com>
2025-10-14 15:39:05 -0400

    python2 problem set

---
+++ python2/ayeayecaptin.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+
+count = 100
+
+if count < 150:
+    print("Aye aye Captain!")
+else:
+    print("Not at all")
+
+number = 25
+
+if number > 0:
+    print("Is positive")
+else:
+    print("Is negative")
---
+++ python2/pos_neg.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+
+number = 25
+
+if number > 0:
+    print("Is positive")
+else:
+    print("Is negative")
