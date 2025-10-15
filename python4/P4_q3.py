0beca628109622a504c61447caca9f7a1b52dada
Walter <worellana09@gmail.com>
2025-10-15 19:58:22 -0400

    P4_q3.py

---
+++ python4/P4_q3.py
@@ -0,0 +1,26 @@
+my_list = ['a','bb','ccc']
+
+list_copy = my_list
+print(list_copy)
+
+#print(my_list)
+
+list_copy2 = my_list.copy()
+print(list_copy2)
+
+my_list2 = ['1','2','3']
+
+print(my_list2)
+
+my_list2.append(4)
+
+print(my_list2)
+
+
+
+
+
+
+
+
+
