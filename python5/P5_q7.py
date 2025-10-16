1483500bc2475fa0f0ef719e3271cf007698c353
Walter <worellana09@gmail.com>
2025-10-16 17:19:02 -0400

    updated answers

--- python5/P5_q7.py
+++ python5/P5_q7.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
+input("type book, song, tree, or organism")
+
 thing = sys.argv[1] 
 fav_things = {'book':'Greenlights','song':'Daddy Yankee - Gasolina','tree':'Purple Haze'}
 
