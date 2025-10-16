fd00c1459d08ce26d63b980f3424056baaf80a7c
Walter <worellana09@gmail.com>
2025-10-16 17:17:51 -0400

    question 8 of p5

---
+++ python5/P5_q8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+fav_things = {'book':'Greenlights','song':'Daddy Yankee - Gasolina','tree':'Purple Haze'}
+
+print(fav_things.keys())
+
+x = input('Choose one of the categories: ') 
+
+
+
+print(fav_things[x])
