6b7731d3c95ea95a30ae4d0d452bb31526af7939
Walter <worellana09@gmail.com>
2025-10-15 20:39:33 -0400

    q4 of python4 problem set

--- python4/P4_q4.py
+++ python4/P4_q4.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
-count = 1
-while count <= 100:
-    print(count)
-    count+=1
+
+num = 1 # we need to initialize the sequence with the number 1
+sum = 0 # we need to initalize the addition. to do this we start with 0 b/c we need add the values. 
+while num <= 100:
+    print(num)
+    sum = sum + num 
+    num+=1
+ 
+
+print(sum)
+
+
+
+
 
 
 
